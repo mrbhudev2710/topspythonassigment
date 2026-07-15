b6fb76078ffc93406229230b3647aebc818c3c76
mrbhudev2710 <manavpurohit44@gmail.com>
2026-07-15 15:32:53 +0530

    commit

---
+++ session2.py
@@ -0,0 +1,31 @@
+# Creating the python variable  also named the delivery_fees, item_price and coupon_discount
+item_price= 270
+delivery_fees= 50  
+coupon_discount= 30 
+
+# Printing the all the list 
+print(item_price)
+print(delivery_fees)
+print(coupon_discount)
+
+
+# Flipkart product price 
+price = 2700
+
+# We have to check the eligible of the free delivery 
+if price > 2000:
+    print("Eligible for free delivery")
+else:
+    print("Delivery charges apply")
+    
+
+
+    # Creating variables for an IPL match 
+team_match = "Royal Challengers Bengaluru"
+run_scored = 218
+overs_played = 20 
+
+# Printing the value 
+print("Team Name:", team_match)
+print("Run Scored:", run_scored)
+print("Overs Played:", overs_played)
